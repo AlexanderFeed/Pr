82f6bf2fc7c031a93d2b613d7f3ccf8f22eb20a4
AlexanderFeed <96307947+AlexanderFeed@users.noreply.github.com>
2022-04-15 20:55:35 +0300

    Add files via upload

--- 11.1.pl
+++ 11.1.pl
@@ -14,6 +14,7 @@ father(X) :- vivod(_,X).
 %wife(X,Y) :- family(X,Y), woman(X).
 w(X,Y) :- woman(X), family(X,Y), write(X), !.
 wife(X) :- w(_,X).
-grand_pa(X,Y) :- ded(X,Y).
-
+grand_da(X,Y) :- ded(X,Y), woman(X), man(Y).
+v(X,Y) :- woman(X), man(Y), ded(X,Y), write(X).
+grand_dats(X) :- v(_,X).
 
