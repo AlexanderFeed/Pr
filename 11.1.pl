0a166e909b752dc7011ad1672c5c93097b2a1167
AlexanderFeed <96307947+AlexanderFeed@users.noreply.github.com>
2022-04-15 21:05:01 +0300

    Add files via upload

--- 11.1.pl
+++ 11.1.pl
@@ -1,12 +1,19 @@
-man(petya).
-man(sasha).
-man(kolya).
-woman(katya).
-woman(masha).
-parent(sasha, katya).
-parent(masha, katya).
-family(sasha, masha).
-family(masha, sasha).
+man(petya). % Дедушка Кати и Коли по отцу
+man(sasha). % Муж
+man(kolya). % Сын мужа и жены
+woman(katya). % Дочь мужа и жены
+woman(masha).% Жена
+woman(luba). %Бабушка Кати и Коли по отцу
+parent(sasha, katya). % Саша отец
+parent(masha, katya). % Маша мать
+parent(sasha, kolya). % Саша отец
+parent(masha, kolya). % Маша мать
+family(sasha, masha). % Саша и Маша женаты
+family(masha, sasha). % Саша и Маша женаты
+bab(luba, katya). %Люба бабушка Кати
+bab(luba, kolya). % Любы бабушка Коли
+bab(katya, luba). %Люба бабушка Кати
+bab(kolya, luba). % Любы бабушка Коли
 ded(katya, petya).%У  Кати дед Петя
 %father(X,Y) :- parent(X,Y), man(X).
 vivod(X,Y) :- parent(X,Y), man(X), write(X), !.
@@ -17,4 +24,5 @@ wife(X) :- w(_,X).
 grand_da(X,Y) :- ded(X,Y), woman(X), man(Y).
 v(X,Y) :- woman(X), man(Y), ded(X,Y), write(X).
 grand_dats(X) :- v(_,X).
+grand_ma_and_son(X,Y) :- bab(X,Y), man(X); bab(X,Y), man(Y), !.
 
