a18866c114edf393773d052434b3d4d921fe1f6c
AlexanderFeed <96307947+AlexanderFeed@users.noreply.github.com>
2022-04-16 00:19:51 +0300

    Готовится

--- 11.1.pl
+++ 11.1.pl
@@ -15,6 +15,7 @@ bab(luba, kolya). % Любы бабушка Коли
 bab(katya, luba). %Люба бабушка Кати
 bab(kolya, luba). % Любы бабушка Коли
 ded(katya, petya).%У  Кати дед Петя
+ded(kolya, petya).%У  Коли дед Петя
 %father(X,Y) :- parent(X,Y), man(X).
 vivod(X,Y) :- parent(X,Y), man(X), write(X), !.
 father(X) :- vivod(_,X).
@@ -26,3 +27,11 @@ v(X,Y) :- woman(X), man(Y), ded(X,Y), write(X).
 grand_dats(X) :- v(_,X).
 grand_ma_and_son(X,Y) :- bab(X,Y), man(X); bab(X,Y), man(Y), !.
 proiz(X,Y) :- Z is X*Y, write(Z).
+proiz_ch(0,Z) :- write(Z).
+proiz_ch(X,Y) :- Z is (Y*(X mod 10)), X1 is X div 10, proiz_ch(X1,Z).
+proverka(X,Y, CH) :- X>Y, max(CH,X).
+max(0,Y) :- Y mod 3 is 0, write(Y), !.
+max(0,_) :- write('Нет кратных 3'), !.
+max(X,Y) :-
+    Z is X mod 10, X1 is X div 10,
+    A is Z mod 3, A is 0, proverka(Z,Y, X1); X1 is X div 10, max(X1,Y).
